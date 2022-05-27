8f00735b6dd2b143689c8a0d185cb7d87d5b7bcd
Sohrab Gheisari <sohrabgheisari@hotmail.com>
2022-05-27 14:24:42 +0430

    init

---
+++ FluentAPI/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FluentAPI.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("STD");
+
+            modelBuilder.Entity<Student>().Property(x => x.Name)
+                .HasMaxLength(150);
+        }
+    }
+
+}
---
+++ FluentAPI/Data/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace FluentAPI.Data
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
